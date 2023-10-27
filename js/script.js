a6f442498fa9ac6397e2393220069d00d84de05b
Faisal Irfan <126792078+Faisalirfan25@users.noreply.github.com>
2023-10-27 11:52:16 +0500

    Updated

--- js/script.js
+++ js/script.js
@@ -25,23 +25,32 @@ function close(){
 
 
 
+let slideIndex = 1; // Initialize the slide index to 1
+showSlides(slideIndex);
 
-let slideIndex = 0;
-showSlides();
+function plusSlides(n) {
+  showSlides((slideIndex += n)); // Increase or decrease the slide index by n
+}
 
-function showSlides() {
+function showSlides(n) {
   let i;
   let slides = document.getElementsByClassName("mySlides");
-  let dots = document.getElementsByClassName("dot");
+
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+
   for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";  
+    slides[i].style.display = "none";
   }
-  slideIndex++;
-  if (slideIndex > slides.length) {slideIndex = 1}    
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";  
-  dots[slideIndex-1].className += " active";
-  setTimeout(showSlides, 3000); // Change image every 2 seconds
+
+  slides[slideIndex - 1].style.display = "block";
 }
+
+// Automatically start the slideshow
+setInterval(function () {
+  plusSlides(1); // Call plusSlides with 1 to move to the next slide
+}, 5000); // Change slide every 3 seconds
